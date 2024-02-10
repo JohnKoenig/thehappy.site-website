0f282b7c697cb4f070293865d5790b042747249a
John Koenig <koenigj3@gmail.com>
2024-02-10 11:02:49 -0500

    clean up

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,17 +4,6 @@ import Foundation
 func routes(_ app: Application) throws {
 
     app.get { req in
-        //let myTitle = "Welcome!"
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US")
-        dateFormatter.timeZone = TimeZone(abbreviation: "EST")
-        dateFormatter.dateStyle = .long
-        dateFormatter.timeStyle = .medium
-        let myDate = dateFormatter.string(from: Date())
-        return req.view.render("standard", ["title": "Swift on Vapor!", "body": myDate])
-    }
-    
-    app.get ("happy") { req in
         let strResponse2 :Set = ["Great!",
                                  "Super!",
                                  "Amazing!",
